156b0486a58d96c2f668bad9deed874752495ef1
SGi4 <h44barznji@outlook.com>
2023-12-08 21:57:48 +0300

    oython changes

---
+++ index.py
@@ -0,0 +1,10 @@
+print("Ha Mazaa")
+
+name = input('Nawe bnusa hai maza:')
+
+if name != 'qa7ba':
+    print("Bas qa7bayatiw lehe")
+else:
+    print("qa7ba xoi anase")
+
+print("barez ",name," gawwadi to xoshi emaya!🗿")        
